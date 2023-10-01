a4b0ccd79bc5f7720151c4840d112c5c954460d3
Ellen Bennett <bennette831@gmail.com>
2023-10-01 04:02:52 -0600

    create INSERT statements to add bears to bears table

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'Brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'F', 'Black', 'energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'White', 'lazy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'F', 'Brown', 'stern', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Black', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'Green', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'Pink', 'sweetie', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, 'M', 'Black', 'solemn', 0);
